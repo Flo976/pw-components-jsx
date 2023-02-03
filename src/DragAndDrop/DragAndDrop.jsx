98a892dba188467b78f39d8cc610883b903dcebe
RivoLink <rivo.link@gmail.com>
2023-02-03 12:55:06 +0300

    DragAndDrop.jsx

---
+++ src/DragAndDrop/DragAndDrop.jsx
@@ -0,0 +1,12 @@
+import classNames from "classnames";
+import Style from "./DragAndDrop.scss?module";
+
+class DragAndDrop {
+	static getMethods() {
+		return {
+        
+		}
+	}
+}
+
+export default DragAndDrop;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Accordion from "./Accordion/Accordion";
 import AccordionObject from "./Accordion/AccordionObject";
 
+import DragAndDrop from "./DragAndDrop/DragAndDrop";
+import DragAndDropObject from "./DragAndDrop/DragAndDropObject";
+
 import Datatable from "./Datatable/Datatable";
 import DatatableObject from "./Datatable/DatatableObject";
 import {determinePagination} from "./Datatable/determinePagination";
@@ -8,6 +11,9 @@ import {determinePagination} from "./Datatable/determinePagination";
 export {
 	Accordion,
     AccordionObject,
+
+	DragAndDrop,
+	DragAndDropObject,
 
 	Datatable,
 	determinePagination,
